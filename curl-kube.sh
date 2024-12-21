d877e5e3074f17bd1158217f1db30b1ff087a80a
Ketut Garjita <77673886+ketut-garjita@users.noreply.github.com>
2024-12-21 20:15:55 +0700

    Update curl-kube.sh

--- curl-kube.sh
+++ curl-kube.sh
@@ -1,3 +1,5 @@
+# Input request example for TensorFlow Serving on Kubernetes via API serve_input_kube.py script
+
 curl -X POST http://localhost:5002/predict \
 -H "Content-Type: application/json" \
 -d '{"text": "Hadiah langsung"}'
