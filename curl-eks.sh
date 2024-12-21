843643f9f235df0b9a277a475c401ee2d40b86c2
Ketut Garjita <77673886+ketut-garjita@users.noreply.github.com>
2024-12-21 20:17:07 +0700

    Update curl-eks.sh

--- curl-eks.sh
+++ curl-eks.sh
@@ -1,3 +1,5 @@
+# Input request example for TensorFlow Serving on Kubernetes via API serve_input_eks.py script
+
 curl -X POST http://localhost:5003/predict \
 -H "Content-Type: application/json" \
 -d '{"text": "Hadiah langsung"}'
