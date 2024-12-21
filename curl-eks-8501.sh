abaf73a8a829ecad637120eb4b10678add68f5c4
Ketut Garjita <77673886+ketut-garjita@users.noreply.github.com>
2024-12-21 20:25:33 +0700

    Delete curl-eks-8501.sh

--- curl-eks-8501.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://a09b1c4049dec41438aaf1b11012942f-1058378139.ap-southeast-3.elb.amazonaws.com:8501/v1/models/saved_model:predict \
--H "Content-Type: application/json" \
--d '{"text": "Hadiah langsung"}'
